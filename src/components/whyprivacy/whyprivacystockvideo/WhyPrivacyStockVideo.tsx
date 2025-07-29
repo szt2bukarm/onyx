d69ece548394ddfa0a12b7fb2fb8b68c0841f95c
Armin <admin@Armins-MacBook-Pro.local>
2025-07-29 19:42:44 +0200

    stock video inline fix

--- src/components/whyprivacy/whyprivacystockvideo/WhyPrivacyStockVideo.tsx
+++ src/components/whyprivacy/whyprivacystockvideo/WhyPrivacyStockVideo.tsx
@@ -1,7 +1,7 @@
 export default function WhyPrivacyStockVideo() {
     return (
-        <div className="h-[600px] w-full">
-            <video src="/stockvideo.mp4" className="w-full h-full object-cover" autoPlay muted loop></video>
+        <div className="h-[300px] md:h-[600px] w-full">
+            <video src="/stockvideo.mp4" className="w-full h-full object-cover pointer-events-none touch-none" autoPlay playsInline muted loop></video>
         </div>
     )
 }
